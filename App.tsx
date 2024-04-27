515aa3f3739ca426c18ae160376a10b6a3481f62
fxfuren <nikitostimoshenko@gmail.com>
2024-04-27 23:05:17 +0300

    secure app

--- App.tsx
+++ App.tsx
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { AuthProvider, useAuth } from './app/context/AuthContext';
+import { NavigationContainer } from '@react-navigation/native';
+import Home from './app/screens/Home';
+import Login from './app/screens/Login';
+import { Button } from 'react-native';
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AuthProvider>
+    </AuthProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+export const Layout = () => {
+  const {authState, onLogout} = useAuth();
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        { authState?.authenticated ? 
+          <Stack.Screen 
+            name="Home" 
+            component={Home}
+            options={{headerRight: () => <Button onPress={onLogout} title='Sign Out' />}} /> 
+          : 
+          <Stack.Screen name="Login" component={Login} />
+        }
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+
