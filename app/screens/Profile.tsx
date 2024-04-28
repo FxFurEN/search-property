0a5418339950717a6584db44be2a4fd4d94bfdfd
fxfuren <nikitostimoshenko@gmail.com>
2024-04-29 00:23:42 +0300

    simple design profile

--- app/screens/Profile.tsx
+++ app/screens/Profile.tsx
@@ -1,10 +1,41 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet,  } from 'react-native'
+import { Button, Input } from 'react-native-elements'
 import React from 'react'
 
 export default function Home() {
   return (
-    <View>
-      <Text>Profile</Text>
+    <View style={styles.container}>
+      <View style={[styles.verticallySpaced, styles.mt20]}>
+        <Input
+          label="Имя"
+          autoCapitalize={'none'}
+        />
+      </View>
+      <View style={styles.verticallySpaced}>
+        <Input
+          label="Почта"
+          autoCapitalize={'none'}
+        />
+      </View>
+      <View style={styles.verticallySpaced}>
+        <Button title="Сохранить" />
+      </View>
     </View>
   )
 }
+
+
+const styles = StyleSheet.create({
+    container: {
+      marginTop: 40,
+      padding: 12,
+    },
+    verticallySpaced: {
+      paddingTop: 4,
+      paddingBottom: 4,
+      alignSelf: 'stretch',
+    },
+    mt20: {
+      marginTop: 20,
+    },
+  })
