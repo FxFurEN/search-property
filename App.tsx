640723ef8b86ed602457de836fe8d012b4b2dc3b
fxfuren <nikitostimoshenko@gmail.com>
2024-05-10 22:38:36 +0300

    redesign add tab nav

--- App.tsx
+++ App.tsx
@@ -6,44 +6,19 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './app/screens/Home';
 import Profile from './app/screens/Profile';
-import { Button, View } from 'react-native';
 import { Session } from '@supabase/supabase-js';
-import { DrawerContentScrollView, DrawerItem, createDrawerNavigator } from '@react-navigation/drawer';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import * as React from 'react-native-reanimated';
 import Register from './app/screens/Register';
 import Login from './app/screens/Login';
+import Ionicons from 'react-native-vector-icons/Ionicons'; 
 
 const Stack = createNativeStackNavigator();
-const Drawer = createDrawerNavigator();
+const Tab = createBottomTabNavigator();
 
 const signOut = async () => {
   await supabase.auth.signOut();
-  };
-
-  const DrawerContent = (props: any) => {
-    return (
-      <>
-      <DrawerContentScrollView {...props}>
-        <DrawerItem
-          label="Главная"
-          onPress={() => props.navigation.navigate('Home')}
-        />  
-        <DrawerItem
-          label="Профиль"
-          onPress={() => props.navigation.navigate('Profile')}
-        /> 
-      </DrawerContentScrollView>
-      <View style={{ borderTopWidth: 1, borderTopColor: '#ddd' }}>
-      <DrawerItem
-        label="Выйти"
-        onPress={signOut}
-      />
-      </View>
-      </>
-      
-    );
-  };
-
+};
 
 export default function App() {
   const [session, setSession] = useState<Session | null>(null)
@@ -55,36 +30,48 @@ export default function App() {
 
     supabase.auth.onAuthStateChange((_event, session) => {
       setSession(session)
-    })
+    });
 
-    
   }, [])
 
-  
-
   return (
-    <NavigationContainer >
+    <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         {session ? (
           <Stack.Screen
-          name="DrawerNavigator" 
-          component={DrawerNavigator} 
-        />
+            name="TabNavigator"
+            component={TabNavigator}
+          />
         ) : (
           <>
             <Stack.Screen name="Login" component={Login} />
             <Stack.Screen name="Register" component={Register} />
           </>
-          
         )}
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
-const DrawerNavigator = () => (
-  <Drawer.Navigator drawerContent={(props) => <DrawerContent {...props} />}>
-    <Drawer.Screen name="Home" component={Home} />
-    <Drawer.Screen name="Profile" component={Profile} />
-  </Drawer.Navigator>
+const TabNavigator = () => (
+  <Tab.Navigator
+    screenOptions={({ route }) => ({
+      tabBarIcon: ({ focused, color, size }) => {
+        let iconName;
+
+        if (route.name === 'Поиск') {
+          iconName = focused ? 'search' : 'search-outline'; 
+        } else if (route.name === 'Profile') {
+          iconName = focused ? 'person' : 'person-outline'; 
+        }
+
+        return <Ionicons name={iconName} size={size} color={color} />;
+      },
+      tabBarActiveTintColor: 'tomato',
+      tabBarInactiveTintColor: 'gray',
+    })}
+  >
+    <Tab.Screen name="Поиск" component={Home} />
+    <Tab.Screen name="Profile" component={Profile} />
+  </Tab.Navigator>
 );
