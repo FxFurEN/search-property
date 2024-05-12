f29a9c4fa2a1383bdf8376a4807c8420f9dde218
fxfuren <nikitostimoshenko@gmail.com>
2024-05-13 00:24:40 +0300

    view markers from db

--- app/screens/Home.tsx
+++ app/screens/Home.tsx
@@ -1,9 +1,9 @@
 import { View, Text, StyleSheet, Alert } from 'react-native'
 import { Button, Input } from 'react-native-elements'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Ionicons from 'react-native-vector-icons/Ionicons'; 
 import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
-
+import { supabase } from '../../lib/supabase'
 
 const INITIAL_REGION={
   latitude: 51.2239,
@@ -17,6 +17,21 @@ const onMarkerPress=()=>{
 }
 
 export default function Home({ navigation }) {
+  const [properties, setProperties] = useState([]);
+
+  useEffect(() => {
+    fetchProperties();
+  }, []);
+
+  const fetchProperties = async () => {
+    const { data, error } = await supabase.from('properties').select('*');
+    if (error) {
+      console.error('Ошибка получения данных:', error.message);
+    } else {
+      setProperties(data);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.row}>
@@ -38,19 +53,35 @@ export default function Home({ navigation }) {
         />
       </View>
       <View style={styles.mapContainer}>
-        <MapView 
-          style={styles.map} 
-          provider={PROVIDER_GOOGLE} 
-          initialRegion={INITIAL_REGION}
-          showsUserLocation
-        >
-          <Marker
-            coordinate={INITIAL_REGION}
-            title="Текущее местоположение"
-            description="Текущее местоположение"
-            onPress={onMarkerPress}
-          />
-        </MapView>
+      <MapView 
+        style={styles.map} 
+        provider={PROVIDER_GOOGLE} 
+        initialRegion={INITIAL_REGION}
+        showsUserLocation
+      >
+      {properties.map((property, index) => {
+            // Парсинг координат из строки JSON
+            const coordinates = JSON.parse(property.coordinates);
+            const latitude = parseFloat(coordinates.latitude);
+            const longitude = parseFloat(coordinates.longitude);
+
+            // Проверка на NaN или невалидные координаты
+            if (!isNaN(latitude) && !isNaN(longitude)) {
+              return (
+                <Marker
+                  key={index}
+                  coordinate={{ latitude, longitude }}
+                  title={property.title}
+                  description={property.description}
+                />
+              );
+            } else {
+              // Вывести предупреждение об ошибке в консоль
+              console.warn(`Некорректные координаты для маркера ${property.title}`);
+              return null;
+            }
+          })}
+      </MapView>
       </View>
     </View>
   )
