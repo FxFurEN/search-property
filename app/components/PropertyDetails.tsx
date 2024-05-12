83d09ee069733836e3495438407a93693fdc3116
fxfuren <nikitostimoshenko@gmail.com>
2024-05-13 02:03:55 +0300

    cut title view

--- app/components/PropertyDetails.tsx
+++ app/components/PropertyDetails.tsx
@@ -31,11 +31,9 @@ export default function PropertyDetails({ isVisible, children, onClose }) {
   return (
     <Modal animationType="slide" transparent={true} visible={isVisible}>
       <View style={[styles.modalContent, { transform: [{ translateY: offset }] }]} {...panResponder.panHandlers}>
-        <View style={styles.titleContainer}>
           <Pressable onPress={onClose}>
             <MaterialIcons name="close" color="#fff" size={22} />
           </Pressable>
-        </View>
         {children}
       </View>
     </Modal>
@@ -46,21 +44,11 @@ const styles = StyleSheet.create({
   modalContent: {
     height: '55%',
     width: '100%',
-    backgroundColor: '#25292e',
+    backgroundColor: 'white',
     borderTopRightRadius: 18,
     borderTopLeftRadius: 18,
     position: 'absolute',
     bottom: 0,
-  },
-  titleContainer: {
-    height: '16%',
-    backgroundColor: '#464C55',
-    borderTopRightRadius: 10,
-    borderTopLeftRadius: 10,
-    paddingHorizontal: 20,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
   },
   title: {
     color: '#fff',
